45fd353bd90bddf51e35910aec46ab80e048bb88
yuk1ty <yuk1ty@users.noreply.github.com>
2024-01-30 09:23:00 +0900

    Add markdown preview plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -16,4 +16,13 @@ return {
     opts = {},
     lazy = false,
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
